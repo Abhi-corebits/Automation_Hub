e41d2b2385b36f03afd721ad4aa5685b7c764cf8
Abhi-corebits <pingleabhinav@gmail.com>
2026-04-10 22:26:47 +0530

    Focusing on imporving mobile experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "iBuiltThis",
   description: "Web projects sharing platform",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
